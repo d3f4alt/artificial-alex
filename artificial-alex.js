9daf45c5a360dee13342e64b4f052e10d753f6de
d3f4alt <d3f4alt@gmail.com>
2017-03-30 23:16:17 -0600

    added ?meme command removed ass from profanity list, as it is in some other words (e.g. pass)

--- artificial-alex.js
+++ artificial-alex.js
@@ -14,18 +14,18 @@ client.on('message', message => {
         message.channel.sendMessage('pong');
     }
 
-    if(message.content === '?avatar') {
-        message.channel.sendMessage(message.author.username + '\'s avatar:');
+    if(message.content.toLowerCase().startsWith('?avatar')) {
+        message.channel.sendMessage(message.author + '\'s avatar:');
         message.channel.sendMessage(message.author.avatarURL);
     }
 
     if(message.content.toLowerCase().startsWith('?clear')) {
         if(Number(message.content.substr(6, message.content.length)) === 0) {
             message.channel.bulkDelete(0);
-            message.channel.sendMessage('Deleted **all** messages');
+            message.channel.sendMessage('Deleted **all** messages.');
         } else {
             message.channel.bulkDelete(Number(message.content.substr(6, message.content.length)));
-            message.channel.sendMessage('Deleted **' + Number(message.content.substr(6, message.content.length)) + '** messages.')
+            message.channel.sendMessage('Deleted **' + Number(message.content.substr(6, message.content.length)) + '** messages.');
         }
     }
 
@@ -57,12 +57,40 @@ client.on('message', message => {
         message.channel.sendMessage('Channel Type: ' + message.channel.type);
     }
 
+    if(message.content.toLowerCase().startsWith('?meme')) {
+        var mes = message.content.substr(6, message.content.length).toLowerCase();
+        mes = mes.replace(' ', '');
+        var newmes = '';
+
+        for(var i = 0; i < mes.length; i++) {
+            if(/^[a-z]+$/i.test(mes.charAt(i))){
+                newmes += ':regional_indicator_' + mes.charAt(i) + ': ';
+            } else {
+                newmes += mes.charAt(i);
+            }
+        }
+
+        for(var a = 0; a < 20; a++) {
+            newmes = newmes.replace('0', ':zero: ');
+            newmes = newmes.replace('1', ':one: ');
+            newmes = newmes.replace('2', ':two: ');
+            newmes = newmes.replace('3', ':three: ');
+            newmes = newmes.replace('4', ':four: ');
+            newmes = newmes.replace('5', ':five: ');
+            newmes = newmes.replace('6', ':six: ');
+            newmes = newmes.replace('7', ':seven: ');
+            newmes = newmes.replace('8', ':eight: ');
+            newmes = newmes.replace('9', ':nine: ');
+        }
+
+        message.channel.sendMessage(newmes);
+    }
+
     if(ProfanityBlock) {
         message.content = message.content.toLowerCase();
 
         if(message.content.indexOf('fuck') >= 0 ||
         message.content.indexOf('shit') >= 0 ||
-        message.content.indexOf('ass') >= 0 ||
         message.content.indexOf('niggers') >= 0 ||
         message.content.indexOf('smd') >= 0 ||
         message.content.indexOf('fck') >= 0 ||
